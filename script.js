1be3b476df100f52023d7d989311a222ec86da9d
Abdirino <abdirinoahash@gmail.com>
2023-07-21 19:23:32 +0300

    new resume added

--- script.js
+++ script.js
@@ -1,72 +1,78 @@
 // on-scroll animation
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        // console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            // entry.target.classList.remove('show')
-        }
-    })
-})
+  entries.forEach((entry) => {
+    // console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      // entry.target.classList.remove('show')
+    }
+  });
+});
 
 const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el))
+hiddenElements.forEach((el) => observer.observe(el));
 // end on-scroll animation
 
 // nav reveal on-scroll
 let prevScroll = window.pageYOffset;
-window.onscroll = function() {
-    let currentScroll = window.pageYOffset;
-    if(prevScroll > currentScroll) {
-        document.getElementsByClassName("navbar").style.top = "0";
-    }else{
-        document.getElementsByClassName("navbar").style.top = "-60px";
-    }
-    prevScroll = currentScroll;
-}
+window.onscroll = function () {
+  let currentScroll = window.pageYOffset;
+  if (prevScroll > currentScroll) {
+    document.getElementsByClassName("navbar").style.top = "0";
+  } else {
+    document.getElementsByClassName("navbar").style.top = "-60px";
+  }
+  prevScroll = currentScroll;
+};
 
 //end nav reveal on-scroll
 
 // popup menu
 let popup = document.getElementById("popup");
-function openPopup () {
-    popup.classList.add("open-popup");
+function openPopup() {
+  popup.classList.add("open-popup");
 }
-function closePopup () {
-    popup.classList.remove("open-popup");
+function closePopup() {
+  popup.classList.remove("open-popup");
 }
 // end popup menu
 
 // scroll to top button
 const toTop = () => {
-    window.scrollTo({top: 0, behavior: "smooth"})
-}
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
 // end scroll to top button
 
 // loader
-var load = document.querySelector(".loader");
-function loadFun(){
-    load.style.display = "none";
-}
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector(".body").style.visibility = "hidden";
+    document.querySelector("#spinner").style.visibility = "visible";
+  } else {
+    document.querySelector("#spinner").style.display = "none";
+    document.querySelector(".body").style.visibility = "visible";
+  }
+};
 // end loader
 
 // contact sec
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxcDxnOXn2Xmgx7C24EnOB-OOu6Iq3gRdzH5WWNUUXos-34NXoi3sCOsx4PtLa-mhpR/exec'
-        const form = document.forms['submit-to-google-sheet']
-        const msg = document.getElementById("msg")
-        form.addEventListener('submit', e => {
-            e.preventDefault()
-            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-                .then(response => {
-                    msg.innerHTML = "Message sent Successfully."
-                    setTimeout(function () {
-                        msg.innerHTML = ""
-                    }, 1000);
-                    form.reset();
-                })
-                .catch(error => console.error('Error!', error.message))
-        })
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxcDxnOXn2Xmgx7C24EnOB-OOu6Iq3gRdzH5WWNUUXos-34NXoi3sCOsx4PtLa-mhpR/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent Successfully.";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 1000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
 // end contact sec
 
 // copyright
@@ -79,9 +85,5 @@ const barMenu = document.querySelector(".checkbtn");
 barMenu.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    barMenu.classList.toggle("active");
+  barMenu.classList.toggle("active");
 }
-
-
-
-
